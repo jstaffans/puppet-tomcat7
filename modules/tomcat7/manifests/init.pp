e04f3c7ac315f7c0930e7b7717e72ad71bad9d4e
Malte S. Stretz <stretz@silpion.de>
2012-10-04 20:02:44 +0000

    Split webapp resource into separate file.

--- modules/tomcat7/manifests/init.pp
+++ modules/tomcat7/manifests/init.pp
@@ -40,16 +40,3 @@ class tomcat7 {
   }   
 
 }
-
-define tomcat7::webapp($path) {
-
-  include tomcat7
-  notice("Establishing http://$hostname:${tomcat7::tomcat7_port}/$name/")
-
-  file { "/var/lib/tomcat7/webapps/${name}.war":
-    owner => 'root',
-    source => $path,
-  }
-
-}
-
