149827b924740e69f703ac04948e88ffbaf7a2c4
Malte S. Stretz <stretz@silpion.de>
2012-10-04 21:24:07 +0000

    Add documentation.

--- modules/tomcat7/manifests/init.pp
+++ modules/tomcat7/manifests/init.pp
@@ -1,3 +1,37 @@
+# = Class: tomcat7
+# 
+# This class installs/configures/manages the Tomcat v7 Java application server.
+# Only supported on Debian-derived OSes.
+# 
+# == Parameters: 
+#
+# $enable::     Whether to start the Tomcat service on boot. Defaults to
+#               true. Valid values: true and false. 
+# $ensure::     Whether to run the Tomcat service. Defaults to running.
+#               Valid values: running and stopped. 
+# $http_port::  Port to configure the HTTP connector with. Defaults to 8080.
+# $https_port:: Port to configure the HTTPS connector with. Defaults to 8443.
+# $jre::        JRE package to depend on. Defaults to 'default'. Suggested
+#               values: 'openjdk-6', 'openjdk-7', 'sun-java6', 'oracle-java7'
+# $install_admin:: Whether to install the tomcat7-admin package. Defaults to
+#                  true. Valid values: true and false.
+# 
+# == Requires: 
+# 
+# Nothing.
+# 
+# == Sample Usage:
+#
+#   class {'tomcat7':
+#     http_port => 80,
+#     jre => 'openjdk-7',
+#   }
+#   class {'tomcat7':
+#     enable => false,
+#     ensure => stopped,
+#   }
+#
+
 class tomcat7 (
     $enable = true,
     $ensure = running,
