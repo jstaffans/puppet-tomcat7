365aecc1e514510cfc5603b6df7aed3fda169a5f
Matthew Blissett <M.Blissett@kew.org>
2013-01-30 11:04:39 +0000

    Turned into a basic Puppet module, removed site.pp etc.

--- cloud-init/src/bootstrap.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -ex
-CACHEDIR=/var/cache/cloud/puppet
-puppet apply --modulepath $CACHEDIR/modules -v $CACHEDIR/site/site.pp
--- test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd $(dirname $0)
-puppet apply --modulepath $PWD/modules -v "$@" $PWD/modules/tomcat7/tests/init.pp
